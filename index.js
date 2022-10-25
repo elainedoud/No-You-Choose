679c283bf6c6caa0c41f9691c9f36fe306f3b581
Elaine <elainedoud@gmail.com>
2022-10-25 13:25:48 -0400

    Successfully pulled randomized result (Brewery) and displayed it

--- index.js
+++ index.js
@@ -1,33 +1,25 @@
-const URL = "https://api.openbrewerydb.org/breweries?by_state=new_york&per_page=3"
+const form = document.querySelector("#form")
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    const cityName = event.target.users_city.value
+    const URL = `https://api.openbrewerydb.org/breweries?by_city=${cityName}&per_page=10`
+    fetch(URL)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        const randomBrewery = data[Math.floor(Math.random()*data.length)]
+        const randomBreweryDisplay = document.createElement('a')
+        const theBrewery = document.querySelector("#result")
+        randomBreweryDisplay.textContent = randomBrewery.name
+        randomBreweryDisplay.href = randomBrewery.website_url
+        theBrewery.append(randomBreweryDisplay)
+        console.log(randomBreweryDisplay)
+    })
+
+})
 
 const zipcodeText = document.querySelector("#users-zipcode")
 const zipcodeSubmit = document.querySelector("#zipcode-submit")
 const giveMeAnotherOne = document.querySelector("#button")
 const switchColorMode = document.querySelector("#color-mode")
-
-fetch(URL)
-.then(res => res.json())
-.then(data => {
-    data.forEach(zipcode => {
-        console.log(zipcode)
-    })
-})
-/*
-function filterResults(zipcode){
-    const results = []
-    const searchText = "10004" //event.target['users-zipcode'].value
-    const dbValue = zipcode.postal_code
-
-    form.addEventListener('submit', (event) => {
-        event.preventDefault()
-        input.value 
-    })
-    for (let i=0; i<zipcode.length; i++){
-        if (zipcode [i][searchText] === dbValue){
-            results.push(zipcode [i])
-        }
-    }
-    */
-    //console.log(results)
-
-    //test
